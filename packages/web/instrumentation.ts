1ca79402b9db33bc734785e24dc16a7088b2b9d6
Alex Zuttre <azuttre@gmail.com>
2026-04-17 13:43:20 +0200

    chore: speed up beta link revalidation

--- packages/web/instrumentation.ts
+++ packages/web/instrumentation.ts
@@ -3,6 +3,8 @@ import * as Sentry from "@sentry/nextjs";
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("./sentry.server.config");
+    const { startLocalBetaLinkCron } = await import("./app/lib/local-beta-link-cron");
+    startLocalBetaLinkCron();
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
